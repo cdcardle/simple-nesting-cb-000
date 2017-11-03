e3813b905c77aac003001ec53e533cb5e49dba77
Chris Cardle <cdcardle@gmail.com>
2017-11-03 13:47:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -93,8 +93,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+  alans_new_info = "GUI"
+	programmer_hash[:alan_kay][:known_for] = alans_new_info
+	return programmer_hash
 
 end
 
@@ -116,5 +117,5 @@ def adding_to_dennis
         }
      }
 	programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-	programmer_hash
+	return programmer_hash
 end
